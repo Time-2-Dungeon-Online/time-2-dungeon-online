6cb4e5e4139bce86d35328e190c5174242112ae4
Jackie Lin <jackie.lin128@gmail.com>
2019-10-08 17:03:56 -0400

    Production webpack functions. Changed app.use for the index.html file to app. get...which seemed to fix the issue where the bundle wasn't getting served.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const db = require('./model/SQLConnection')
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Coming soon...')
-});
+const PORT = 3000;
 
-// app.get('/api', (req, res) => {
-//   res.send('Working? Plz')
-// })
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('listening');
-})
+app.use('/', express.static(path.resolve(__dirname, '../')));
+''
+// //serve things inside of the build file
+app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+
+
+//only serve static files 
+// if(process.env.NODE_ENV === 'production') {
+//   console.log('we are in production');
+// };
+
+//global error handler
+// app.use('*', (err, req, res) => {
+//   console.log('There was a route error, this is the global error handler in server.js')
+// });
+
+app.listen(3000, () => console.log('listening on PORT: ' + PORT));
