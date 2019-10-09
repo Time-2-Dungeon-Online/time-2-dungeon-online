214f0dbb031100b51f4eb93f85b12455ee141fca
Michelle Herrera <mesherrera@aol.com>
2019-10-08 20:32:45 -0400

    Resolved issues with inhandled promises. All get and post request working.

--- __test__/user-routes-supertest.test.js
+++ __test__/user-routes-supertest.test.js
@@ -1,18 +1,15 @@
 const request = require('supertest');
-import app from '../server';
+const server = require('../server/server');
 
 describe('Route integration', () => {
-  let session = null;
   describe('/', () => {
     describe('GET', () => {
-      it('responds with 200 status code and text/HTML content', () => {
-        request(app)
+      test('responds with 200 status code and text/HTML content', () => {
+        return request(server)
         .get('/')
-        .expect(200)
-        .expect('Content-Type', /text\/html/)
-        .end((err, res) => {
-          if (err) return done(err);
-          done();
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.header.contentType).toBe(/text\/html/);
         });
       });
     });
@@ -20,122 +17,67 @@ describe('Route integration', () => {
 
   describe('/signup', () => {
     describe('GET', () => {
-      it('responds with 200 status code.', () => {
-        request(app)
+      test('responds with 200 status code.', () => {
+        return request(server)
           .get('/signup')
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            done();
+          .then((response) => {
+            expect(response.statusCode).toBe(200);
           });
       });
     });
 
     describe('POST', () => {
-      it('responds with 200 status code and application/json content type', () => {
-        request(app)
+      test('responds with 200 status code and application/json content type', () => {
+        return request(server)
           .post('/signup')
           .send({ username: 'Tester', password: 'helloworld'})
           .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            done();
-          });
+          .then((response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.header.contentType).toBe(/json/);
+          }));
       });
 
-      it('return username with on response body', () => {
-          request(app)
+      test('return username with on response body', () => {
+          return request(server)
           .post('/signup')
           .send({ username: 'Tester', password: 'helloworld'})
           .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((res) => {
-            assert(res.body.username, username)
-            done();
-          })
-          .catch(err => {
-            if (err) done(err);
+          .then((response) => {
+            expect(response.body.username).toBe('Tester');
           })
       });
-
-      it('set a session cookie when the user has completing the sign up process.', () => {
-        request(app)
-        .post('/signup')
-        .send({ username: 'Tester', password: 'helloworld'})
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-          session = res.header.cookie;
-          done();
-        })
-        .catch((err) => {
-          if (err) return done(err); 
-        })
-      })
     });
   });
 
   describe('/login', () => {
     describe('GET', () => {
-      it('responds with 200 status code', () => {
-        request(app)
+      test('responds with 200 status code', () => {
+        return request(server)
         .get('/login')
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          done();
-        });
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+        })
       });
     });
 
     describe('POST', () => {
-      it('responds with 200 status code and application/json content type', () => {
-        request(app)
+      test('responds with 200 status code and application/json content type', () => {
+        return request(server)
           .post('/login')
-          .send({ username: 'Tester', password: 'helloworld'})
           .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            done();
+          .then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.header.contentType).toBe(/text\/html/);
           });
       });
 
-      it('return username with on response body', () => {
-          request(app)
+      test('return username with on response body', () => {
+          return request(server)
           .post('/login')
-          .send({ username: 'Tester', password: 'helloworld'})
           .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((res) => {
-            assert(res.body.username, username)
-            done();
-          })
-          .catch(err => {
-            if (err) done(err);
-          });
-      });
-
-      it('set a session cookie when the user has completing the sign up process.', () => {
-        request(app)
-        .post('/login')
-        .send({ username: 'Tester', password: 'helloworld'})
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-          session = res.header.cookie;
-          done();
-        })
-        .catch((err) => {
-          if (err) return done(err); 
-        });
+          .send({ username: 'Tester', password: 'helloworld'})
+          .then(response => expect(response.body.username).toBe('Tester'));
       });
     });
   });
