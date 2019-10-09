24baada6b99f03ec3e721118acaf7af90d8f8744
Jackie Lin <jackie.lin128@gmail.com>
2019-10-09 12:00:43 -0400

    changed some of the import routes inside of the test files

--- __test__/user-routes-supertest.test.js
+++ __test__/user-routes-supertest.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const server = require('../server/server');
+const server = require('../server/server.js');
 
 describe('Route integration', () => {
   describe('/', () => {
