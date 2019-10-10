1f5b9bed7dc535201c3607234e9d101780b3e600
Michelle Herrera <mesherrera@aol.com>
2019-10-09 20:54:28 -0400

    Modified export.

--- server/server.js
+++ server/server.js
@@ -16,10 +16,9 @@ if(process.env.NODE_ENV === 'production') {
 
 
 // global error handler
-app.use('*', (err, req, res) => {
-  console.log('There was a route error, this is the global error handler in server.js')
-});
+// app.use('*', (err, req, res) => {
+//   console.log('There was a route error, this is the global error handler in server.js')
+// });
 
-app.listen(3000, () => console.log('listening on PORT: ' + PORT));
+module.exports = app.listen(3000, () => console.log('listening on PORT: ' + PORT));
 
-module.exports = app;
