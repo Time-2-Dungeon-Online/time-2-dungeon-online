c908dfcfa2421d0de5c72b160ed6572df63e7f87
Michelle Herrera <mesherrera@aol.com>
2019-10-10 00:32:02 -0400

    Facebook authentication routes and Passport setup.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const db = require("./model/SQLConnection");
-const path = require("path");
 const passport = require("passport")
-const facebookStrategy = ('./config/passport');
+const FacebookStrategy = require("passport-facebook").Strategy;
+const pool = require('./model/SQLConnection');
+const auth = require('./config/auth');
+// const path = require("path");
+
+// const strategy = ('./config/passport');
 
 const PORT = 3000;
 
@@ -15,24 +18,41 @@ const PORT = 3000;
 // }
 
 app.use(express.json());
+passport.initialize();
+
+passport.use(new FacebookStrategy({
+    clientID: auth.clientID,
+    clientSecret: auth.clientSecret,
+    callbackURL: auth.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    const { email } = profile.email
+    const values = [accessToken, refreshToken, profile];
+    const query =
+      "INSERT VALUES ($1, $2, $3) INTO users(accessToken, refreshToken, email)";
+    pool
+      .query(query, values)
+      .then(response => done(null, response.rows[0]))
+      .catch(err => console.log(err));
+}));
+
 app.get("/");
 app.get('/secret', (req, res) => {
-  res.send('../secret.html');
+  res.send('SECRET!!!!!');
 });
 
 
 // Redirect the user to Facebook for authentication.  When complete, Facebook will redirect the user back to the application.
-app.get("/auth/facebook", passport.authenticate("facebook", {scope: ['email']}));
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
 
 // Facebook will redirect the user to this URL after approval.  Finish the authentication process by attempting to obtain an access token.
 // If access was granted, the user will be logged in.  Otherwise, authentication has failed.
-app.get(
-  "/auth/facebook/secret",
-  passport.authenticate("facebook", {
-    successRedirect: "/secret",
-    failureRedirect: "/"
-  })
-);
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/secret', failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/secret');
+  });
+
 
 // global error handler
 app.use("*", (err, req, res) => {
