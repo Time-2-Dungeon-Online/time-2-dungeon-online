2f5d1215c1d281da093107a8f4a3c27fc51976ba
Michelle Herrera <mesherrera@aol.com>
2019-10-10 00:31:03 -0400

    Facebook API information.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   clientID: '390263538580772',
   clientSecret: 'fbcdd606fa96d18342d93f3447ec7d0a',
-  callbackURL: "http://localhost:8080/auth/facebook/secret",
+  callbackURL: "https://localhost:8080/auth/facebook/callback",
   profileFields: ['email'],
 };
