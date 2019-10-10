283f0182f33fb9c80c9829957eef20aa502ece9a
Will Magee <wmagee03@gmail.com>
2019-10-10 17:59:35 -0400

    added attack enemy logic and added alive property to enemy object

--- server/model/Player.js
+++ server/model/Player.js
@@ -1,13 +1,18 @@
 module.exports = class Player {
+
   constructor (playerName) {
     this.playerName = playerName;
     this.cardCount = 40;
     this.alive = true;
     this.currentHand = null;
   }
+
   updateCardCount (amount, instruction) {
     if (instruction === 'decrease') this.hp -= amount;
     if (instruction === 'increase') this.hp += amount;
     if (this.hp < 0) this.hp = 0;
   }
+
+  
+
 }
