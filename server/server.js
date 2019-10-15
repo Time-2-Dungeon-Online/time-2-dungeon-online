e53d3ba372dca745ffd8a308d0af645eb875c1cc
David Chai <davidchai717@gmail.com>
2019-10-14 23:38:21 -0400

    Slight backend change

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const path = require('path');
 
 const PORT = 3000;
 
+serverVisualizer.start(app)();
+
 app.use(express.json());
 
 // Establish the WebSocket server
@@ -22,10 +24,10 @@ if(process.env.NODE_ENV === 'production') {
 };
 
 
-// global error handler
-app.use('*', (err, req, res) => {
-  console.log('There was a route error, this is the global error handler in server.js')
-});
+// // global error handler
+// app.use('*', (err, req, res) => {
+//   console.log('There was a route error, this is the global error handler in server.js')
+// });
 
 app.listen(3000, () => console.log('listening on PORT: ' + PORT));
 
